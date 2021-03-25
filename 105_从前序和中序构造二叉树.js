852c816ee4f9b69b214568375441cacc14852184
cxblovecw <873587091@qq.com>
2021-03-25 10:29:31 +0800

    update

--- 105_从前序和中序构造二叉树.js
+++ 105_从前序和中序构造二叉树.js
@@ -24,25 +24,25 @@ function TreeNode(val, left, right) {
 /**
  * @param {number[]} preorder
  * @param {number[]} inorder
- * @return {TreeNode}
+ * @return {TreeNode|any}
  */
-var buildTree = function(preorder, inorder) {
-  // 当中序数组为空时 返回null (上层是叶子节点)
-  if(inorder.length===0) return null;
-  // 获取前序数组的第一个元素 并删除 
-  // 前序数组的第一个元素 代表跟元素 通过递归 就可以对应到每个子树的根节点
-  // 删除是为了让节点不断的往下移动
-  let mid=preorder.splice(0,1)[0];
-  // 获取中序数组的根节点的index 将其分成左右两部分
-  let midIndex=inorder.findIndex((item)=>mid==item);
-  // 创建根节点
-  let root=new TreeNode(mid);
-  // 前序和中序构造 先构造左子树 
-  // 节点的左子树
-  root.left=buildTree(preorder,inorder.slice(0,midIndex));
-  // 节点的右子树
-  root.right=buildTree(preorder,inorder.slice(midIndex+1))
-  return root;
+var buildTree = function (preorder, inorder) {
+	// 当中序数组为空时 返回null (上层是叶子节点)
+	if (inorder.length === 0) return null;
+	// 获取前序数组的第一个元素 并删除
+	// 前序数组的第一个元素 代表跟元素 通过递归 就可以对应到每个子树的根节点
+	// 删除是为了让节点不断的往下移动
+	let mid = preorder.splice(0, 1)[0];
+	// 获取中序数组的根节点的index 将其分成左右两部分
+	let midIndex = inorder.findIndex((item) => mid == item);
+	// 创建根节点
+	let root = new TreeNode(mid);
+	// 前序和中序构造 先构造左子树
+	// 节点的左子树
+	root.left = buildTree(preorder, inorder.slice(0, midIndex));
+	// 节点的右子树
+	root.right = buildTree(preorder, inorder.slice(midIndex + 1));
+	return root;
 };
 
-console.log(buildTree([3,9,20,15,7],[9,3,15,20,7]));
+console.log(buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]));
--- 106_从中序和后序构造二叉树.js
+++ 106_从中序和后序构造二叉树.js
@@ -24,25 +24,25 @@ function TreeNode(val, left, right) {
 /**
  * @param {number[]} inorder
  * @param {number[]} postorder
- * @return {TreeNode}
+ * @return {TreeNode|any}
  */
-var buildTree = function(inorder,postorder) {
-  // 当中序数组为空时 返回null (上层是叶子节点)
-  if(inorder.length===0) return null;
-  // 获取前序数组的第一个元素 并删除 
-  // 前序数组的第一个元素 代表跟元素 通过递归 就可以对应到每个子树的根节点
-  // 删除是为了让节点不断的往下移动
-  let mid=postorder.splice(postorder.length-1)[0];
-  // 获取中序数组的根节点的index 将其分成左右两部分
-  let midIndex=inorder.findIndex((item)=>mid==item);
-  // 创建根节点
-  let root=new TreeNode(mid);
-  // 中序和后序构造 先构造右子树 
-  // 节点的右子树
-  root.right=buildTree(inorder.slice(midIndex+1),postorder)
-  // 节点的左子树
-  root.left=buildTree(inorder.slice(0,midIndex),postorder);
-  return root;
+var buildTree = function (inorder, postorder) {
+	// 当中序数组为空时 返回null (上层是叶子节点)
+	if (inorder.length === 0) return;
+	// 获取前序数组的第一个元素 并删除
+	// 前序数组的第一个元素 代表跟元素 通过递归 就可以对应到每个子树的根节点
+	// 删除是为了让节点不断的往下移动
+	let mid = postorder.splice(postorder.length - 1)[0];
+	// 获取中序数组的根节点的index 将其分成左右两部分
+	let midIndex = inorder.findIndex((item) => mid == item);
+	// 创建根节点
+	let root = new TreeNode(mid);
+	// 中序和后序构造 先构造右子树
+	// 节点的右子树
+	root.right = buildTree(inorder.slice(midIndex + 1), postorder);
+	// 节点的左子树
+	root.left = buildTree(inorder.slice(0, midIndex), postorder);
+	return root;
 };
 
-console.log(buildTree([9,3,15,20,7],[9,15,7,20,3]));
+console.log(buildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]));
